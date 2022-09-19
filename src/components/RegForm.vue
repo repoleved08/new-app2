1d71d32fc36204dfb3e7afb2f4d428bd28183a19
repoleved08 <geekbii08@gmail.com>
2022-09-19 16:41:04 +0300

    first commit

--- src/components/RegForm.vue
+++ src/components/RegForm.vue
@@ -1,13 +1,5 @@
 <template>
   <v-app>
-    <v-card-text>Hell world</v-card-text>
-    <br />
-    <div v-for="ccc in ccc" v-bind:key="ccc.id">
-      <p>Hey</p>
-      <h2>Hello {{ ccc.first_name }}</h2>
-      <p>{{ ccc.last_name }}</p>
-    </div>
-    <div></div>
     <v-row justify="center">
       <v-dialog v-model="dialog" persistent max-width="600px">
         <template v-slot:activator="{ on, attrs }">
@@ -24,21 +16,22 @@
                   <v-text-field label="Legal first name*" required></v-text-field>
                 </v-col>
                 <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    label="Legal middle name"
-                    hint="example of helper text only on focus"
-                  ></v-text-field>
+                  <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
                 </v-col>
                 <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    label="Legal last name*"
-                    hint="example of persistent helper text"
-                    persistent-hint
-                    required
-                  ></v-text-field>
+                  <v-text-field label="Legal last name*" hint="example of persistent helper text" persistent-hint
+                    required></v-text-field>
                 </v-col>
-                <v-col cols="12">
+                <v-col cols="12" sm="6" md="4">
                   <v-text-field label="Email*" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field label="Legal last name*" hint="example of persistent helper text" persistent-hint
+                    required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field label="Place of Birth" hint="example of persistent helper text" persistent-hint
+                    required></v-text-field>
                 </v-col>
                 <v-col cols="12">
                   <v-text-field label="Password*" type="password" required></v-text-field>
@@ -47,21 +40,17 @@
                   <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
                 </v-col>
                 <v-col cols="12" sm="6">
-                  <v-autocomplete
-                    :items="[
-                      'Skiing',
-                      'Ice hockey',
-                      'Soccer',
-                      'Basketball',
-                      'Hockey',
-                      'Reading',
-                      'Writing',
-                      'Coding',
-                      'Basejump',
-                    ]"
-                    label="Interests"
-                    multiple
-                  ></v-autocomplete>
+                  <v-autocomplete :items="[
+                    'Skiing',
+                    'Ice hockey',
+                    'Soccer',
+                    'Basketball',
+                    'Hockey',
+                    'Reading',
+                    'Writing',
+                    'Coding',
+                    'Basejump',
+                  ]" label="Interests" multiple></v-autocomplete>
                 </v-col>
               </v-row>
             </v-container>
@@ -69,8 +58,8 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-            <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+            <v-btn class="warning" text @click="dialog = false">Close</v-btn>
+            <v-btn class="success" text @click="dialog = false">Save</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -90,7 +79,6 @@ export default {
         const response = await this.$http.get(
           "http://adt-test.nascop.org/ADT/public/api/patients/{ccc}"
         );
-        // JSON responses are automatically parsed.
         this.ccc = response.data;
       } catch (error) {
         console.log(error);
